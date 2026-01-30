a4a331dd1101742465cec48c48a7252ae2024c00
Sergii <svtomusjak@gmal.com>
2026-01-30 16:02:08 +0200

    remove old themes

--- wordpress/wp-content/theme-geoplast/inc/template/block-title.php
+++
@@ -1,4 +0,0 @@
-<<?php the_sub_field('h'); ?>
- class="block-title ts-<?php the_sub_field('title-sze'); ?> ts-sm-<?php the_sub_field('title-sze_mobile'); ?>">
-<?php the_sub_field('title'); ?>
-</<?php the_sub_field('h'); ?>>
